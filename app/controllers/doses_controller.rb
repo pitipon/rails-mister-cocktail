dec6803be267ea7c4fd4958cdbda97e8c81332f5
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-11-23 18:20:42 +0800

    finish add more ingredient to cocktail

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,3 +1,38 @@
 class DosesController < ApplicationController
-  
+  before_action :set_cocktail, only: [:new, :create]
+  before_action :set_ingredient, only: [:create]
+  before_action :all_ingredients, only: [:new]
+
+  def new
+    # @cocktail in our `simple_form_for`
+    # @ingredients : to list all ingredient and let user choose
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def set_cocktail
+      @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def all_ingredients
+      @ingredients = Ingredient.all
+  end
+
+  def set_ingredient
+      @ingredient = Ingredient.find(params["dose"]["ingredient_id"])
+  end
+
+  def dose_params
+      params.require(:dose).permit(:description, :quantity)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,29 @@ Rails.application.routes.draw do
 
   root to: "cocktails#index"
   resources :ingredients
-  resources :cocktails
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#
-#   Prefix Verb       URI Pattern                     Controller#Action
-# ingredients GET         /ingredients(.:format)          ingredients#index
-#        POST   /ingredients(.:format)          ingredients#create
-# new_ingredient GET    /ingredients/new(.:format)      ingredients#new
-# edit_ingredient GET    /ingredients/:id/edit(.:format) ingredients#edit
-# ingredient GET    /ingredients/:id(.:format)      ingredients#show
-#        PATCH  /ingredients/:id(.:format)      ingredients#update
-#        PUT    /ingredients/:id(.:format)      ingredients#update
-#        DELETE /ingredients/:id(.:format)      ingredients#destroy
-# cocktails GET    /cocktails(.:format)            cocktails#index
-#        POST   /cocktails(.:format)            cocktails#create
-# new_cocktail GET    /cocktails/new(.:format)        cocktails#new
-# edit_cocktail GET    /cocktails/:id/edit(.:format)   cocktails#edit
-# cocktail GET    /cocktails/:id(.:format)        cocktails#show
-#        PATCH  /cocktails/:id(.:format)        cocktails#update
-#        PUT    /cocktails/:id(.:format)        cocktails#update
-#        DELETE /cocktails/:id(.:format)        cocktails#destroy
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+#   Prefix Verb   URI Pattern                                 Controller#Action
+#     root GET    /                                           cocktails#index
+# ingredients GET    /ingredients(.:format)                      ingredients#index
+#          POST   /ingredients(.:format)                      ingredients#create
+# new_ingredient GET    /ingredients/new(.:format)                  ingredients#new
+# edit_ingredient GET    /ingredients/:id/edit(.:format)             ingredients#edit
+# ingredient GET    /ingredients/:id(.:format)                  ingredients#show
+#          PATCH  /ingredients/:id(.:format)                  ingredients#update
+#          PUT    /ingredients/:id(.:format)                  ingredients#update
+#          DELETE /ingredients/:id(.:format)                  ingredients#destroy
+# cocktail_doses POST   /cocktails/:cocktail_id/doses(.:format)     doses#create
+# new_cocktail_dose GET    /cocktails/:cocktail_id/doses/new(.:format) doses#new
+# cocktails GET    /cocktails(.:format)                        cocktails#index
+#          POST   /cocktails(.:format)                        cocktails#create
+# new_cocktail GET    /cocktails/new(.:format)                    cocktails#new
+# edit_cocktail GET    /cocktails/:id/edit(.:format)               cocktails#edit
+# cocktail GET    /cocktails/:id(.:format)                    cocktails#show
+#          PATCH  /cocktails/:id(.:format)                    cocktails#update
+#          PUT    /cocktails/:id(.:format)                    cocktails#update
+#          DELETE /cocktails/:id(.:format)                    cocktails#destroy
 
 
 end
