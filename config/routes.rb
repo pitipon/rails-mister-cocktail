5e3e0f22616d9a1101aa56b195d91e8df9c83197
Pitipon Pluemworasawat <pitipon@gmail.com>
2017-11-23 17:39:21 +0800

    add root at route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root to: "cocktails#index"
   resources :ingredients
   resources :cocktails
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
